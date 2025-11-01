ad0d70291c4489e0828ca2d7572946e51132516a
Michel Promonet <michel.promonet@urbanandmainlines.com>
2025-11-01 23:17:06 +0000

    build in debug

--- install_on_system.sh
+++ install_on_system.sh
@@ -1,7 +1,7 @@
 echo "Deploying ingescape library and its dependencies on the system"
 git submodule update --init --recursive #make sure all dependecies are here
 mkdir build
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DOSX_UNIVERSAL=ON -DWITH_DEPS=ON
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DOSX_UNIVERSAL=ON -DWITH_DEPS=ON
 make -j8 -C build
 sudo make -j8 -C build install
 sudo rm -Rf build
